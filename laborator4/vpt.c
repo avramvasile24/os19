0065702be0efda51a1739a3fef53963664bb4b0b
Avram Vasile <avramvasile24@yahoo.com>
2019-03-21 19:18:04 +0200

    Actualizare III

---
+++ laborator4/vpt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+double heron(double a, double b, double c)
+{
+	double p=(a+b+c)/2;
+	double x=p*(p-a)*(p-b)*(p-c);
+	return sqrt(x);
+}
+double volumpt(double h, double a, double b, double c)
+{
+	double Ab=heron(a,b,c);
+	return (Ab*h)/3;
+}
+int main( int argc, char *argv[])
+{
+	if(argc!=5)
+	{
+		printf("Eroare, Folositi: vpt h, a, b, c\n");
+		return -1;
+	}
+	double h=atof(argv[1]);
+	double a=atof(argv[2]);
+	double b=atof(argv[3]);
+	double c=atof(argv[4]);
+	double v=volumpt(h,a,b,c);
+	printf("%f\n",v);
+	return 0;
+}
